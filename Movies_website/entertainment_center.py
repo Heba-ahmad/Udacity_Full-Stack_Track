c0b54ec175b920fbad49a9fb89796511241e023b
Heba-Ahmad-Saada <hebasaada@msn.com>
2018-08-11 21:01:06 +0300

    create instances of class movie

---
+++ Movies_website/entertainment_center.py
@@ -0,0 +1,22 @@
+import media
+
+Arrietty = media.Movie("Arrietty", "Arrietty, a tiny teenager, lives with her parents in the recesses of a suburban home, unbeknown to the homeowner and housekeeper. Like others of her kind, Arrietty remains hidden from her human hosts, but occasionally ventures forth from beneath the floorboards to borrow sugar cubes and other supplies. A secret friendship forms when 12-year-old Shawn meets Arrietty, but their relationship could spell danger for Arrietty's family.", "http://t3.gstatic.com/images?q=tbn:ANd9GcSKt62ofk-ko3kXHFOcqO5ZYjEhbO8itYTVSYvHPpNWUgeQ-VI_", "https://www.youtube.com/watch?v=VlMe7PavaRQ")
+
+beauty_and_the_beast = media.Movie("Beauty and the Beast", "An arrogant young prince and his castle\'s servants fall under the spell of a wicked enchantress, who turns him into Beast until he learns to love and be loved in return.", "http://www.reelviews.net/resources/img/posters/thumbs/beauty_and_the_beast_poster.jpg", "https://www.youtube.com/watch?v=tRlzmyveDHE")
+
+Big_Hero_6 = media.Movie("Big Hero 6", "Robotics prodigy Hiro lives in the city of San Fransokyo. Next to his older brother, Tadashi, Hiro\'s closest companion is Baymax, a robot whose sole purpose is to take care of people. When a devastating turn of events throws Hiro into the middle of a dangerous plot, he transforms Baymax and his other friends, Go Go Tamago, Wasabi, Honey Lemon and Fred into a band of high-tech heroes.", "https://i.pinimg.com/564x/9e/79/36/9e79367940bcf3cb20da5b6854132097.jpg", "https://www.youtube.com/watch?v=z3biFxZIJOQ")
+
+The_Book_of_Eli  = media.Movie("The Book of Eli ", "A post-apocalyptic tale, in which a lone man fights his way across America in order to protect a sacred book that holds the secrets to saving humankind.", "https://m.media-amazon.com/images/M/MV5BNTE1OWI1YzgtZjEyMy00MjQ4LWE0NWMtYTNhYjc0ZDQ3ZGRkXkEyXkFqcGdeQXVyNDQ2MTMzODA@._V1_SY1000_CR0,0,666,1000_AL_.jpg", "https://www.youtube.com/watch?v=Vn1D1KUy584")
+
+The_Devil_Wears_Prada = media.Movie("The Devil Wears Prada", "Andy is a recent college graduate with big dreams. Upon landing a job at prestigious Runway magazine, she finds herself the assistant to diabolical editor Miranda Priestly. Andy questions her ability to survive her grim tour as Miranda's whipping girl without getting scorched.", "http://www.impawards.com/2006/posters/devil_wears_prada.jpg", "https://www.youtube.com/watch?v=LG0xYJJbko8")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://images-na.ssl-images-amazon.com/images/I/61OUGpUfAyL._SY879_.jpg", "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+beauty_and_the_beast = media.Movie("Beauty and the Beast", "An arrogant young prince and his castle\'s servants fall under the spell of a wicked enchantress, who turns him into Beast until he learns to love and be loved in return.", "http://www.reelviews.net/resources/img/posters/thumbs/beauty_and_the_beast_poster.jpg", "https://www.youtube.com/watch?v=tRlzmyveDHE")
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://images-na.ssl-images-amazon.com/images/I/91q0UP6%2BUTL._SY879_.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://images-na.ssl-images-amazon.com/images/I/61OUGpUfAyL._SY879_.jpg", "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+
+
